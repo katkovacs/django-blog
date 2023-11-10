598131deb6debca5bde65b2eca807eec6c78a6ab
Katalin Kovacs <katalin.kovacs@solirius.com>
2023-11-10 08:37:11 +0000

    Setting up pythoneverywhere

--- social/settings.py
+++ social/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['katkovacs.pythonanywhere.com', '127.0.0.1']
 INTERNAL_IPS = ['127.0.0.1']
 
 
