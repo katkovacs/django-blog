4c9be26208ae4b35863e40e3146044c5cb56044a
Katalin Kovacs <katalin.kovacs@solirius.com>
2023-11-10 09:17:21 +0000

    Add Post fields to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Post)
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user', 'message']
